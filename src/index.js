ca0899157466600dba9f5e9da0996a64f0434adc
James Madden <jamesmadden@Jamess-Air.vc.shawcable.net>
2020-08-26 16:57:14 -0700

    Change background colour on scroll for overscroll

--- src/index.js
+++ src/index.js
@@ -2,4 +2,17 @@ import "core-js/stable";
 import "regenerator-runtime/runtime";
 
 import './components/root.js';
-import '@pwabuilder/pwainstall'
+import '@pwabuilder/pwainstall';
+
+let isDown = false;
+
+window.addEventListener('scroll', () => {
+
+  if (window.scrollY > 132 && !isDown) {
+    isDown = true;
+    document.body.style.background = '#192E61';
+  } else if (window.scrollY <= 132 && isDown) {
+    isDown = false;
+    document.body.style.background = '#203A7D linear-gradient(45deg, #192E61, #192E61)';
+  }
+});
